8f00c2a8bed638c183e957a20c58b04fb9908489
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-10-07 10:56:30 +0200

    Cambio mensaje

---
+++ src/boletin2_3/Boletin2_3.java
@@ -0,0 +1,20 @@
+
+
+package boletin2_3;
+
+import java.util.Scanner;
+
+
+public class Boletin2_3 {
+
+   
+     public static void main(String[] args) {
+        float lado;
+        Scanner obx=new Scanner(System.in);
+        System.out.println(" teclea lado cuadrado: ");
+        lado=obx.nextFloat();
+        System.out.println("area = "+lado*lado);
+       
+    }
+    
+}
